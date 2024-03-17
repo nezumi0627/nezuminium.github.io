045de6adb1200e4178dd8c3138b78c178e75bbb0
ねずみくん <103794002+nezumi0627@users.noreply.github.com>
2024-03-17 18:53:46 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,44 @@
-document.addEventListener("DOMContentLoaded", async () => {
+document.addEventListener('DOMContentLoaded', async () => {
   try {
     await liff.init({ liffId: '2004151744-pBd3JMLd' });
 
-    document.getElementById('sendBtn').addEventListener('click', async () => {
-      try {
-        const profile = await liff.getProfile();
-        const displayName = profile.displayName;
-        const userId = profile.userId;
-
-        const message = `
-言語設定：ja-JP
-LIFF SDKのバージョン：${liff.getVersion()}
-LINE内ブラウザで動作させているか：${liff.isInClient()}
-ログインしているか：${liff.isLoggedIn()}
-OS：${liff.getOS()}
-アクセストークン：${liff.getAccessToken()}
-画面（1対1のチャット、グループ、またはトークルーム）を一意に識別する値：${JSON.stringify(liff.getContext())}
-ユーザー名等：
-ユーザー名：${displayName}
-ユーザーID：${userId}`;
-
-        sendLiff(message);
-      } catch (err) {
-        console.error('Error:', err);
-      }
-    });
+    const sendBtn = document.getElementById('sendBtn');
+    sendBtn.addEventListener('click', handleSendClick);
   } catch (err) {
     console.error('Error:', err);
   }
 });
 
+async function handleSendClick() {
+  try {
+    const profile = await liff.getProfile();
+    const { displayName, userId } = profile;
+    const text = createText(profile);
+    const icon = document.getElementById('user-icon');
+    icon.src = profile.pictureUrl;
+    icon.alt = profile.displayName;
+    const name = document.querySelector('.user-name');
+    name.textContent = displayName;
+    sendLiff(text);
+  } catch (err) {
+    console.error('Error:', err);
+  }
+}
+
+function createText(profile) {
+  const { liffLanguage, liffIsInClient, liffIsLoggedIn, liffOs, liffAccessToken, liffContext } = liff;
+  return `${liffLanguage}\n${liffIsInClient}\n${liffIsLoggedIn}\n${liffOs}\n${liffAccessToken}\n${JSON.stringify(liffContext)}\nユーザー名：${profile.displayName}、ユーザーID：${profile.userId}`;
+}
+
 function sendLiff(text) {
   liff.sendMessages([{
     type: 'text',
-    text: text,
+    text,
     sentBy: {
-      label: "ねずみ",
+      label: "Nezumi-Project",
       iconUrl: "https://raw.githubusercontent.com/nezumi0627/nezuminium.github.io/main/giphy.gif",
       linkUrl: "https://github.com/nezumi0627"
     }
-  }]).then(function () {
-    liff.closeWindow();
-  });
+  }]).then(() => liff.closeWindow());
 }
+
