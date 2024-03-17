72d6387a702efa854a1d14ec16ffa05e376602b4
ねずみくん <103794002+nezumi0627@users.noreply.github.com>
2024-03-17 18:39:56 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,65 +1,41 @@
-// liffの初期化とDOMContentLoadedイベントのリスナー
-document.addEventListener("DOMContentLoaded", async () => {
-    try {
-      await liff.init({ liffId: '<LIFF_ID>' });
-  
-      // 「sendBtn」ボタンのクリックイベントリスナーを設定
-      document.getElementById('sendBtn').addEventListener('click', async () => {
-        try {
-          const profile = await liff.getProfile();
-          const displayName = profile.displayName;
-          const userId = profile.userId;
-  
-          // プロフィール情報とLIFF情報を取得してメッセージとして送信
-          const message = `
-  ○言語設定
-  liff.getLanguage()=${liff.getLanguage()}
-  
-  ○LIFF SDKのバージョン
-  liff.getVersion()=${liff.getVersion()}
-  
-  ○LINE内ブラウザで動作させているかどうか
-  liff.isInClient()=${liff.isInClient()}
-  
-  ○ログインしているかどうか
-  liff.isLoggedIn()=${liff.isLoggedIn()}
-  
-  ○OS
-  liff.getOS()=${liff.getOS()}
-  
-  ○アクセストークン
-  liff.getAccessToken()=${liff.getAccessToken()}
-  
-  ○画面（1対1のチャット、グループ、またはトークルーム）を一意に識別する値
-  liff.getContext()=${JSON.stringify(liff.getContext())}
-  
-  ○ユーザー名等
-  ユーザー名:${displayName}
-  ユーザーID:${userId}`;
-  
-          // 取得した情報をメッセージとして送信
-          sendLiff(message);
-        } catch (err) {
-          document.getElementById('log').innerText = 'getProfile ng\n' + err;
-        }
-      });
-    } catch (err) {
-      document.getElementById('log').innerText = 'init ng\n' + err;
-    }
-  });
-  
-  // メッセージを送信する関数
-  function sendLiff(text) {
-    liff.sendMessages([{
-      type: 'text',
-      text: text,
-      sentBy: {
-        label: "ねずみ",
-        iconUrl: "https://raw.githubusercontent.com/nezumi0627/nezuminium.github.io/main/giphy.gif",
-        linkUrl: "https://github.com/nezumi0627"
+document.addEventListener('DOMContentLoaded', async () => {
+  try {
+    await liff.init({ liffId: '2004151744-pBd3JMLd' });
+
+    const sendBtn = document.getElementById('sendBtn');
+    sendBtn.addEventListener('click', async () => {
+      try {
+        const profile = await liff.getProfile();
+        const { displayName, userId } = profile;
+
+        const message = `
+      言語設定：${liff.getLanguage()}
+      LIFF SDKのバージョン：${liff.getVersion()}
+      LINE内ブラウザで動作させているか：${liff.isInClient()}
+      ログインしているか：${liff.isLoggedIn()}
+      OS：${liff.getOS()}
+      アクセストークン：${liff.getAccessToken()}
+      画面（1対1のチャット、グループ、またはトークルーム）を一意に識別する値：${JSON.stringify(liff.getContext())}
+      ユーザー名等：ユーザー名：${displayName}、ユーザーID：${userId}`;
+
+        sendLiff(message);
+      } catch (err) {
+        console.error('Error:', err);
       }
-    }]).then(function () {
-      liff.closeWindow();
     });
+  } catch (err) {
+    console.error('Error:', err);
   }
-  
+});
+
+function sendLiff(text) {
+  liff.sendMessages([{
+    type: 'text',
+    text,
+    sentBy: {
+      label: "ねずみ",
+      iconUrl: "https://raw.githubusercontent.com/nezumi0627/nezuminium.github.io/main/giphy.gif",
+      linkUrl: "https://github.com/nezumi0627"
+    }
+  }]).then(() => liff.closeWindow());
+}
